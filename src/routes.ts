a6c784c5a81b02144b4584798250ac08517ddbf1
Mboscolo1 <mathewsboscolo@outlook.com>
2020-06-12 14:36:32 -0300

    Updated routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,48 @@
-import express from 'express'
+import express from 'express';
+import { celebrate, Joi} from 'celebrate';
 
-const routes = express.Router()
 
-routes.get('/', (request, response) => {
-    return response.json({message: "Hello World"})
-})
+import multer from 'multer';
+import multerConfig from './config/multer'
+
+import PointsController from './controllers/PointsController';
+import ItemsController from './controllers/ItemsController';
+
+
+const routes = express.Router();
+
+const upload = multer(multerConfig);
+
+const pointsController = new PointsController();
+const itemsController = new ItemsController();
+
+routes.get('/items', itemsController.index);
+routes.get('/points', pointsController.index);
+routes.get('/points/:id', pointsController.show);
+
+
+routes.post(
+    '/points', 
+    upload.single('image'),
+    celebrate({
+        body: Joi.object().keys({
+            name: Joi.string().required(),
+            email: Joi.string().required().email(),
+            whatsapp: Joi.number().required(),
+            latitude: Joi.number().required(),
+            longitude: Joi.number().required(),
+            city: Joi.string().required(),
+            uf: Joi.string().required().max(2),
+            itens: Joi.string().required(),
+
+        })
+    }, {
+    
+        abortEarly: false
+
+    }),
+
+    pointsController.create
+);
 
 export default routes;
